6e6e2ac5a878a0582ea704c6ace53557447d46df
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-11-14 22:11:47 -0300

    adicionando a URL e retornando uma lista, mas foi filtrado o nome e retornado o valor da chave balance, como mostra ao imprimir.

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,21 @@
-
+import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() {
- print("Olá Mundo");
+  requestData();
+}
+
+requestData() {
+  String url =
+      "https://gist.githubusercontent.com/jacksondgmedeiros/d8fe863d121c830c943b67dd5c295d52/raw/812cf69296ec730c0b31878871fd805d1bbefc44/accounts.json";
+
+  Future<Response> futureResponse = get(Uri.parse(url));
+  futureResponse.then((Response response) {
+    print(response.body);
+    List<dynamic> listsAccount = json.decode(response.body);
+    Map<String, dynamic> mapCarla = listsAccount.firstWhere((element) => element["name"] == "Carla",);
+    print(mapCarla["balance"]);
+
+  },);
+
 }
