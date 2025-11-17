393d4d0d5c5d40565362b149b9059fe13b9f80c6
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-11-16 22:34:16 -0300

    criando uma nova função para desmostrar o uso de funcções com async e await, onde é esperado o retorno da solicitação para seguir com o código

--- bin/main.dart
+++ bin/main.dart
@@ -3,19 +3,34 @@ import 'dart:convert';
 
 void main() {
   requestData();
+  requestDataAsync();
 }
 
-requestData() {
+void requestData() {
   String url =
       "https://gist.githubusercontent.com/jacksondgmedeiros/d8fe863d121c830c943b67dd5c295d52/raw/812cf69296ec730c0b31878871fd805d1bbefc44/accounts.json";
 
+// Future de response, onde o get mostra todos os metadados da url
   Future<Response> futureResponse = get(Uri.parse(url));
+//   do resultado, mostra apenas o body, todo o json, sem filters
   futureResponse.then((Response response) {
     print(response.body);
+
+    // converte o json em string, mapeia como chave, valor, fazer a pesquisa atraves de uma função anonima com a pesquisa e retorna o valor de balance da Carka
     List<dynamic> listsAccount = json.decode(response.body);
-    Map<String, dynamic> mapCarla = listsAccount.firstWhere((element) => element["name"] == "Carla",);
+    Map<String, dynamic> mapCarla = listsAccount.firstWhere((element) => element["name"] == "Elisa",);
     print(mapCarla["balance"]);
 
   },);
+
+}
+
+
+void requestDataAsync() async {
+  String url =
+      "https://gist.githubusercontent.com/jacksondgmedeiros/d8fe863d121c830c943b67dd5c295d52/raw/812cf69296ec730c0b31878871fd805d1bbefc44/accounts.json";
+  Response response = await get(Uri.parse(url));
+  print(json.decode(response.body)[0]);
+  print("ultima linha despois de aguardar a função async");
 
 }
