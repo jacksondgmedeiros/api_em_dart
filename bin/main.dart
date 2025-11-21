f9dfdb9ab2bd7cbb3a13cdba50ad40bdf5382b50
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-11-21 15:45:54 -0300

    criando uma nova função para enviar dados para a minha API usando o método POST, adicionando cabeçalhos, também foi criado uma api-key para o gist com autorizaç~]ao para realizar alterações, adicionado a chave em um arquivo local e adicionado no gitignore para não ser vazado

--- bin/main.dart
+++ bin/main.dart
@@ -1,36 +1,65 @@
+import 'package:api_em_dart/api-key.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 
 void main() {
   requestData();
   requestDataAsync();
+  senDataAsync({
+    "id": "NEW001",
+    "name": "Joao",
+    "lastName": "Medeiros",
+    "balance": 500,
+  });
 }
 
 void requestData() {
   String url =
       "https://gist.githubusercontent.com/jacksondgmedeiros/d8fe863d121c830c943b67dd5c295d52/raw/812cf69296ec730c0b31878871fd805d1bbefc44/accounts.json";
 
-// Future de response, onde o get mostra todos os metadados da url
+  // Future de response, onde o get mostra todos os metadados da url
   Future<Response> futureResponse = get(Uri.parse(url));
-//   do resultado, mostra apenas o body, todo o json, sem filters
+  //   do resultado, mostra apenas o body, todo o json, sem filters
   futureResponse.then((Response response) {
     print(response.body);
 
     // converte o json em string, mapeia como chave, valor, fazer a pesquisa atraves de uma função anonima com a pesquisa e retorna o valor de balance da Carka
     List<dynamic> listsAccount = json.decode(response.body);
-    Map<String, dynamic> mapCarla = listsAccount.firstWhere((element) => element["name"] == "Elisa",);
+    Map<String, dynamic> mapCarla = listsAccount.firstWhere(
+      (element) => element["name"] == "Elisa",
+    );
     print(mapCarla["balance"]);
-
-  },);
-
+  });
 }
 
-
-void requestDataAsync() async {
+Future<List<dynamic>> requestDataAsync() async {
   String url =
       "https://gist.githubusercontent.com/jacksondgmedeiros/d8fe863d121c830c943b67dd5c295d52/raw/812cf69296ec730c0b31878871fd805d1bbefc44/accounts.json";
   Response response = await get(Uri.parse(url));
-  print(json.decode(response.body)[0]);
-  print("ultima linha despois de aguardar a função async");
+  return json.decode(response.body);
+}
 
+void senDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listsAccount = await requestDataAsync();
+  listsAccount.add(mapAccount);
+  String content = json.encode(listsAccount);
+
+  String url = "https://api.github.com/gists/d8fe863d121c830c943b67dd5c295d52";
+  Response response = await post(
+    Uri.parse(url),
+    headers: {
+      "Authorization" : "Bearer $gitHubKey"
+    },
+    body: json.encode({
+      "description": "accounts.json",
+      "public": true,
+      "files":{
+        "accounts.json":{
+          "content": content,
+        }
+      }
+    }),
+    );
+
+  print(response.statusCode);
 }
